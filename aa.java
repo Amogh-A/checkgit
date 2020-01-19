6ee43306e56577e2cf88606746ec23efd7296a3f
Amogh <ammu.131998@gmail.com>
2020-01-19 14:26:28 +0530

    Initial commit

---
+++ aa.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void public static void main(String[] args) 
+	{
+		System.out.println("This is git");
+		System.out.println("hell");		
+	}
+}
